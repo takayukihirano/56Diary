16d80334ecbd427fc77b7fc0e1abe1ce7ce364d5
平野貴之 <>
2019-11-15 11:39:06 +0800

    edit作成

--- diary/Diary/routes/web.php
+++ diary/Diary/routes/web.php
@@ -17,3 +17,7 @@ Route::get('/diary/create','DiaryController@create')->name('diary.create');
 // Route::get('/diary/create','DiaryController@create')->('好きな名前');
 Route::post('/diary/store','DiaryController@store')->name('diary.store');
 Route::delete('/diary/{id}','DiaryController@destroy')->name('diary.destroy');
+
+Route::get('/diary/{id}/edit','DiaryController@edit')->name('diary.edit');
+
+Route::put('/diary/{id}/update','DiaryController@update')->name('diary.update');
