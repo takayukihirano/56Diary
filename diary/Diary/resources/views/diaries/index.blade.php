f79e902359dc4bb17a5187b65b4d97d02546c9b7
平野貴之 <>
2019-11-22 09:20:43 +0800

    いいね機能追加

--- diary/Diary/resources/views/diaries/index.blade.php
+++ diary/Diary/resources/views/diaries/index.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">新規投稿</a>
 @foreach ($diaries as $diary)
-    <div class="m-4 p-4 border border-primary">
+  <div class="m-4 p-4 border border-primary">
     <p>{{$diary->title}}</p>
     <p>{{$diary->body}}</p>
     <p>{{$diary->created_at}}</p>
@@ -21,6 +21,21 @@
         <button class='btn btn-danger'>削除</button>
         </form>
     @endif
+    <div class="mt-3 ml-3">
+
+        @if (Auth::check() && $diary->likes->contains(function ($user){
+            return $user->id === Auth::user()->id;
+        }))
+            {{-- ログインしている かつ この日記にいいねしている場合 --}}
+            <i class="fas fa-heart fa-lg text-danger js-dislike"></i>
+
+        @else
+            <i class="far fa-heart fa-lg text-danger js-like"></i>
+
+        @endif
+        <input type="hidden" class="diary-id" value="{{ $diary->id }}">
+    <span class="js-like-num">{{ $diary->likes->count() }}</span>
     </div>
+  </div>
 @endforeach
 @endsection
